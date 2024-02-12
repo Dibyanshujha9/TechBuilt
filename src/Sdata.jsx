2a41c98e9cb039ef26bbef0c134480987210bce9
Dibyanshujha9 <jhadibyanshu46@gmail.com>
2024-02-12 09:12:04 +0530

    first commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,41 @@
+import web from "../src/images/s1.jpg";
+import app from "../src/images/app.jpeg";
+import android from "../src/images/android.jpeg";
+import digital from "../src/images/digital.jpeg";
+import marketing from "../src/images/marketing.jpg";
+import software from "../src/images/software.jpeg";
+
+const Sdata = [
+  {
+    imgsrc: web,
+    title: "web development",
+    CardTitle: "Enance your Web Development Experience"
+  },
+  {
+    imgsrc: app,
+    title: "App development",
+    CardTitle : "Enance your App Development Experience"
+  },
+  {
+    imgsrc: software,
+    title: "Software development",
+    CardTitle : "Enance your Software Development Experience"
+  },
+  {
+    imgsrc: digital,
+    title: "Digital Marketing",
+    CardTitle : "Enance your Digital Marketing Experience"
+  },
+  {
+    imgsrc: android,
+    title: "Android development",
+    CardTitle : "Enance your Android Development Experience"
+  },
+  {
+    imgsrc: marketing,
+    title: "Marketing",
+    CardTitle : "Enance your Marketing Experience"
+  },
+];
+
+export default Sdata;
